6c9d74723483f10dee6f89ec7db285f61e35992b
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-05-13 13:42:17 +0530

    Update MuRAGAppLatency.py

--- MuRAGAppLatency.py
+++ MuRAGAppLatency.py
@@ -218,8 +218,6 @@ if uploaded_file is not None:
                                                                                             
     
     
-
-    @st.cache_data()
     def encode_image(image_path):
       """Getting the base64 string"""
       with open(image_path, "rb") as image_file:
@@ -291,9 +289,9 @@ if uploaded_file is not None:
     st.write(f"{bullet_point} Summary generation process completed")  
         
     
-    @st.cache_resource()
+    
     def create_multi_vector_retriever(
-      _vectorstore, text_summaries, texts, table_summaries, tables, image_summaries, images
+      vectorstore, text_summaries, texts, table_summaries, tables, image_summaries, images
     ):
       """
       Create retriever that indexes summaries, but returns raw images or texts
@@ -305,7 +303,7 @@ if uploaded_file is not None:
     
       # Create the multi-vector retriever
       retriever = MultiVectorRetriever(
-          vectorstore=_vectorstore,
+          vectorstore=vectorstore,
           docstore=store,
           id_key=id_key,
       )
@@ -340,65 +338,65 @@ if uploaded_file is not None:
 
     
     def looks_like_base64(sb):
-      """Check if the string looks like base64"""
-      return re.match("^[A-Za-z0-9+/]+[=]{0,2}$", sb) is not None
-    
+          """Check if the string looks like base64"""
+          return re.match("^[A-Za-z0-9+/]+[=]{0,2}$", sb) is not None
+        
     
     def is_image_data(b64data):
-      """
-      Check if the base64 data is an image by looking at the start of the data
-      """
-      image_signatures = {
-          b"\xFF\xD8\xFF": "jpg",
-          b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A": "png",
-          b"\x47\x49\x46\x38": "gif",
-          b"\x52\x49\x46\x46": "webp",
-      }
-      try:
-          header = base64.b64decode(b64data)[:8]  # Decode and get the first 8 bytes
-          for sig, format in image_signatures.items():
-              if header.startswith(sig):
-                  return True
-          return False
-      except Exception:
-          return False
+          """
+          Check if the base64 data is an image by looking at the start of the data
+          """
+          image_signatures = {
+              b"\xFF\xD8\xFF": "jpg",
+              b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A": "png",
+              b"\x47\x49\x46\x38": "gif",
+              b"\x52\x49\x46\x46": "webp",
+          }
+          try:
+              header = base64.b64decode(b64data)[:8]  # Decode and get the first 8 bytes
+              for sig, format in image_signatures.items():
+                  if header.startswith(sig):
+                      return True
+              return False
+          except Exception:
+              return False
     
     def resize_base64_image(base64_string, size=(128, 128)):
-      """
-      Resize an image encoded as a Base64 string
-      """
-      # Decode the Base64 string
-      img_data = base64.b64decode(base64_string)
-      img = Image.open(io.BytesIO(img_data))
-    
-      # Resize the image
-      resized_img = img.resize(size, Image.LANCZOS)
-    
-      # Save the resized image to a bytes buffer
-      buffered = io.BytesIO()
-      resized_img.save(buffered, format=img.format)
-      # Encode the resized image to Base64
-      return base64.b64encode(buffered.getvalue()).decode("utf-8")
+          """
+          Resize an image encoded as a Base64 string
+          """
+          # Decode the Base64 string
+          img_data = base64.b64decode(base64_string)
+          img = Image.open(io.BytesIO(img_data))
+        
+          # Resize the image
+          resized_img = img.resize(size, Image.LANCZOS)
+        
+          # Save the resized image to a bytes buffer
+          buffered = io.BytesIO()
+          resized_img.save(buffered, format=img.format)
+          # Encode the resized image to Base64
+          return base64.b64encode(buffered.getvalue()).decode("utf-8")
     
     #context creation
     def split_image_text_types(docs):
-      """
-      Split base64-encoded images and texts
-      """
-      b64_images = []
-      texts = []
-      for doc in docs:
-          # Check if the document is of type Document and extract page_content if so
-          if isinstance(doc, Document):
-              doc = doc.page_content
-          if looks_like_base64(doc) and is_image_data(doc):
-              doc = resize_base64_image(doc, size=(1300, 600))
-              b64_images.append(doc)
-          else:
-              texts.append(doc)
-      if len(b64_images) > 0:
-          return {"images": b64_images[:1], "texts": []}
-      return {"images": b64_images, "texts": texts}
+          """
+          Split base64-encoded images and texts
+          """
+          b64_images = []
+          texts = []
+          for doc in docs:
+              # Check if the document is of type Document and extract page_content if so
+              if isinstance(doc, Document):
+                  doc = doc.page_content
+              if looks_like_base64(doc) and is_image_data(doc):
+                  doc = resize_base64_image(doc, size=(1300, 600))
+                  b64_images.append(doc)
+              else:
+                  texts.append(doc)
+          if len(b64_images) > 0:
+              return {"images": b64_images[:1], "texts": []}
+          return {"images": b64_images, "texts": texts}
     
     
     #response generation
